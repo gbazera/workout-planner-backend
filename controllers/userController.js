7a18fe784be038b194cf9e5e3681a6fdb4cf6661
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-21 00:57:00 +0400

    added signup

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const User = require('../models/userModel')
+
+const loginUser = async (req, res) => {
+    res.json({ msg: 'login usr' })
+}
+
+const signupUser = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({ email, user })
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+}
+
+module.exports = { loginUser, signupUser }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 const routineRoutes = require('./routes/routines')
+const userRoutes = require('./routes/user')
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use((req, res, next)=>{
 })
 
 app.use('/api/routines', routineRoutes)
+app.use('/api/user', userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
