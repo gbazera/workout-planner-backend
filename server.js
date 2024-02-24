fbafe48f648b81b41de9045bcade622075e4a50b
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-24 23:56:10 +0400

    Minor fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user')
 const app = express();
 
 app.use(cors({
-    origin: 'https://workout-planner-gb.netlify.app/',
+    origin: 'https://workout-planner-gb.netlify.app',
     credentials: true
 }))
 
